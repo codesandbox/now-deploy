c905cac2692598802ad71257c3cc5999c484af27
Sara Vieira <hey@iamsaravieira.com>
2019-02-07 11:38:59 +0100

    add get deployments endpoint

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { send, json, buffer } = require('micro')
+const { send, json } = require('micro')
 const { router, get, post, del } = require('microrouter')
 const axios = require('axios')
 const microCors = require('micro-cors')
@@ -7,6 +7,15 @@ const makeHeaders = require('./utils/makeHeaders')
 const cors = microCors()
 
 const url = `https://api.zeit.co`
+
+const getAliasCall = async (id, token) => {
+  const { data } = await axios.get(
+    `${url}/v2/now/deployments/${id}/aliases`,
+    makeHeaders(token)
+  )
+
+  return data
+}
 
 const alias = async (req, res) => {
   const { token } = req.query
@@ -30,10 +39,7 @@ const getAlias = async (req, res) => {
   const { token } = req.query
   const { id } = req.params
   try {
-    const { data } = await axios.get(
-      `${url}/v2/now/deployments/${id}/aliases`,
-      makeHeaders(token)
-    )
+    const data = await getAliasCall(id, token)
 
     send(res, 200, data)
   } catch (e) {
@@ -43,6 +49,7 @@ const getAlias = async (req, res) => {
 
 const deleteDeployment = async (req, res) => {
   const { token } = req.query
+  // micro adds a trailing slash
   const id = req.params.id.split('/')[0]
   try {
     const { data } = await axios.delete(
@@ -56,10 +63,40 @@ const deleteDeployment = async (req, res) => {
   }
 }
 
+const getDeployments = async (req, res) => {
+  const { token, name } = req.query
+  try {
+    const {
+      data: { deployments }
+    } = await axios.get(`${url}/v3/now/deployments`, makeHeaders(token))
+
+    const deploysNoAlias = deployments
+      .filter(d => d.name === name)
+      .sort((a, b) => (a.created < b.created ? 1 : -1))
+
+    const assignAlias = async deployment => {
+      const alias = await getAliasCall(deployment.uid, token)
+      return {
+        ...deployment,
+        alias: alias.aliases
+      }
+    }
+
+    const sandboxAlias = await deploysNoAlias.map(assignAlias)
+    const deploys = await Promise.all(sandboxAlias)
+
+    send(res, 200, { deploys })
+  } catch (e) {
+    console.log(e)
+    send(res, 500, { error: 'There was a problem getting your aliases' })
+  }
+}
+
 module.exports = cors(
   router(
     post('/alias/:id', alias),
     get('/alias/:id', getAlias),
+    get('/deployments', getDeployments),
     del('/deployments/:id', deleteDeployment)
   )
 )
